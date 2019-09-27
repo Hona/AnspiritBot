0ca0cc826ccbae5359f49e210e41c0e33f8f0df6
Luke Parker <lukemparker@outlook.com>
2019-09-27 14:41:48 +1000

    Added a zone name overview imaged for quick reference

---
+++ AnspiritConsoleUI/Commands/GeneralModule.cs
@@ -0,0 +1,18 @@
+﻿using AnspiritConsoleUI.Constants;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnspiritConsoleUI.Commands
+{
+    public class GeneralModule : AnspiritModuleBase
+    {
+        [Command("zoneoverview")]
+        public async Task SendZoneOverviewAsync()
+        {
+            await Context.Channel.SendFileAsync(DiscordConstants.ZoneOverviewFilePath);
+        }
+    }
+}
--- AnspiritConsoleUI/Constants/DiscordConstants.cs
+++ AnspiritConsoleUI/Constants/DiscordConstants.cs
@@ -6,6 +6,7 @@ namespace AnspiritConsoleUI.Constants
     internal static class DiscordConstants
     {
         internal static readonly string ConfigFolderPath = Path.Combine(Environment.CurrentDirectory, "config");
+        internal static readonly string ResourceFolderPath = Path.Combine(Environment.CurrentDirectory, "resources");
         internal static readonly string DiscordTokenFilePath = Path.Combine(ConfigFolderPath, "discordToken.txt");
         internal static readonly string GoogleAPIKeyFilePath = Path.Combine(ConfigFolderPath, "googleapikey.txt");
         internal static readonly string AnzacSpiritSheetsID = Path.Combine(ConfigFolderPath, "sheetsID.txt");
@@ -13,6 +14,7 @@ namespace AnspiritConsoleUI.Constants
         internal static readonly string AnzacSpiritWarOfficers = Path.Combine(ConfigFolderPath, "authorisedWarOfficers.txt");
         internal static readonly string AnzacSpiritPlayers = Path.Combine(ConfigFolderPath, "players.txt");
         internal static readonly string DatabaseConnectionStringPath = Path.Combine(ConfigFolderPath, "database.txt");
+        internal static readonly string ZoneOverviewFilePath = Path.Combine(ResourceFolderPath, "ZoneOverview.png");
         internal static readonly char DiscordCommandPrefix = '!';
         internal const int LogPaddingLength = 10;
     }
